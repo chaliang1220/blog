2c2ef3f07cbe904db8d4a00c18177f3c98c1e4fe
Ziven <chaliang1220@163.com>
2023-04-12 20:47:16 +0800

    1

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 header('Content-type: text/html; charset=utf-8');
 ini_set("error_reporting", "E_ALL & ~E_NOTICE");
 
-echo "Hi，Webhooks！123123  1231231 By Ziven Hallo <br/>";
+echo "Hi，Webhoo1231231ks！123123  1231231 By Ziven Hallo <br/>";
 
 echo '<br/>测试：输出项目路径和用户目录：你好啊blog!<br/>';
 
