30b05397e738b02a641de07df616774ba1aba71b
Ziven <chaliang1220@163.com>
2023-04-12 18:22:20 +0800

    1

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ ini_set("error_reporting", "E_ALL & ~E_NOTICE");
 
 echo "Hi，Webhooks！123123  1231231 By Ziven 1213123 <br/>";
 
-echo '<br/>测试：输出项目路径和用户目录：<br/>';
+echo '<br/>测试：输出项目路径和用户目录：你好啊blog!<br/>';
 
 exec("cd ~ && cd - && cd -", $output);
 
