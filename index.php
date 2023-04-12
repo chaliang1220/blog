d8736d7dde9617179fb725afd96782a27bcc2ac5
Ziven <chaliang1220@163.com>
2023-04-12 17:44:46 +0800

    updata:测试webhook

--- index.php
+++ index.php
@@ -4,8 +4,6 @@ ini_set("error_reporting", "E_ALL & ~E_NOTICE");
 
 echo "Hi，Webhooks！By Ziven<br/>";
 
-echo "<br/><a href='https://www.aliyun.com/minisite/goods?taskPkg=1111ydsrwb&pkgSid=11388&recordId=1033318&userCode=is4kfbdt'>新用户购买服务器优惠活动🔥</a>";
-
 echo '<br/>测试：输出项目路径和用户目录：<br/>';
 
 exec("cd ~ && cd - && cd -", $output);
